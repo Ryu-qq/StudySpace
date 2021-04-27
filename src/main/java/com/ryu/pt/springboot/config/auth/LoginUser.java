4589e798c4fbf60428eadaafc574a4e7f4f6465e
Ryu-qq <fbtkdals2@naver.com>
2021-04-27 23:34:22 +0900

    Page 290까지 완료

---
+++ src/main/java/com/ryu/pt/springboot/config/auth/LoginUser.java
@@ -0,0 +1,13 @@
+package com.ryu.pt.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+
+
+}
