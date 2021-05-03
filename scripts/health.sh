c09a5229000ebdb441e85fec918083bbd95332d7
Ryu-qq <fbtkdals2@naver.com>
2021-05-03 22:34:42 +0900

    무중단배포 nginx 적용

--- scripts/health.sh
+++ scripts/health.sh
@@ -29,11 +29,11 @@ do
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health check 실패 왜!. "
+    echo "> Health check 실패.. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
-  echo "> Health check 연결 실패. 재시도..."
+  echo "> Health check 연결 실패. 재시도... 와이~"
   sleep 10
 done
