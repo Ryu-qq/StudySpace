8b08e73e657d52727d306e2450928d1bd44f8ac4
Ryu-qq <fbtkdals2@naver.com>
2021-12-01 00:00:22 +0900

    bean validation groups Form 전송객체 분리 bean validation Http message 컨버터

---
+++ src/main/java/hello/itemservice/web/validation/Form/ItemUpdateForm.java
@@ -0,0 +1,25 @@
+package hello.itemservice.web.validation.Form;
+
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 100000)
+    private Integer price;
+    //수정에서는 자유롭게 수정
+    private Integer quantity;
+}
